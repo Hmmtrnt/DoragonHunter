de63191b1ca81de392c76a4050d30f8c96ed3eac
Hmmtrnt <rento5310@gmail.com>
2023-11-27 02:16:52 +0900

    シーン遷移追加

--- MonsterHunter/Assets/Game/Debug/Scenes/DebugSceneScript/DebugSceneTransition.cs
+++ MonsterHunter/Assets/Game/Debug/Scenes/DebugSceneScript/DebugSceneTransition.cs
@@ -3,6 +3,34 @@ using UnityEngine.SceneManagement;
 
 public class DebugSceneTransition : MonoBehaviour
 {
+    private ControllerManager controllerManager;
+
+    private void Start()
+    {
+        controllerManager = GetComponent<ControllerManager>();
+    }
+
+    private void Update()
+    {
+        if(controllerManager._YButtonDown)
+        {
+            //DebugPlayScene();
+        }
+        if(controllerManager._XButtonDown)
+        {
+            //TitleScene();
+        }
+        if(controllerManager._AButtonDown)
+        {
+            MainScene();
+        }
+        if(controllerManager._BButtonDown)
+        {
+            //ResultScene();
+        }
+    }
+
+
     public void DebugPlayScene()
     {
         SceneManager.LoadScene("DebugPlayScene");

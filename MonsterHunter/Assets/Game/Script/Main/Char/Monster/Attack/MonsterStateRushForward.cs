a7675a5828cc3473f0c167a77400bf673ec022d5
Hmmtrnt <rento5310@gmail.com>
2023-12-29 00:51:48 +0900

    プレイヤーの方向を向き突進してくる処理追加

--- MonsterHunter/Assets/Game/Script/Main/Char/Monster/Attack/MonsterStateRushForward.cs
+++ MonsterHunter/Assets/Game/Script/Main/Char/Monster/Attack/MonsterStateRushForward.cs
@@ -21,22 +21,29 @@ public partial class MonsterState
         {
             if (owner._stateFlame <= 30)
             {
-                owner._trasnform.position -= Vector3.forward * 0.15f;
+                //owner._trasnform.position -= Vector3.forward * 0.15f;
+                owner._forwardSpeed = 0.15f;
             }
             else if (owner._stateFlame <= 130)
             {
-                owner._trasnform.position -= Vector3.forward * 0.5f;
+                //owner._trasnform.position -= Vector3.forward * 0.5f;
                 owner._rushCollisiton.SetActive(true);
                 owner._wingRightCollisiton.SetActive(true);
                 owner._wingLeftCollisiton.SetActive(true);
+                owner._forwardSpeed = 0.5f;
             }
             else if (owner._stateFlame <= 160)
             {
-                owner._trasnform.position += Vector3.forward * 0.15f;
+                //owner._trasnform.position += Vector3.forward * 0.15f;
                 owner._rushCollisiton.SetActive(false);
                 owner._wingRightCollisiton.SetActive(false);
                 owner._wingLeftCollisiton.SetActive(false);
+                owner._forwardSpeed = -0.15f;
             }
+
+            owner._trasnform.LookAt(owner._hunter.transform.position);
+
+            owner._trasnform.position += owner._moveVelocity;
         }
 
         public override void OnExit(MonsterState owner, StateBase nextState)

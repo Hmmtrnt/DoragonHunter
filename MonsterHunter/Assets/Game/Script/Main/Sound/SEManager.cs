6b91a330fdfb0c069f7e54162be44f81b90c7299
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2024-01-15 11:19:57 +0900

    音の処理の追加

---
+++ MonsterHunter/Assets/Game/Script/Main/Sound/SEManager.cs
@@ -0,0 +1,25 @@
+/*SEマネージャー*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SEManager : MonoBehaviour
+{
+    // SE素材.
+    public AudioClip[] _audio;
+    // 発音
+    private AudioSource _audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ MonsterHunter/Assets/Game/Script/Main/Sound/SoundManager.cs
@@ -0,0 +1,20 @@
+/*‰¹º‘fŞ‚ÌŠÇ—*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    enum BGM
+    {
+        BGMNum// BGM”.
+    }
+
+    enum SE
+    {
+        Slash,// aŒ‚‰¹.
+        RoundSlash,// ‹Cn‘å‰ñ“]a‚è.
+        SENum// SE”.
+    }
+}

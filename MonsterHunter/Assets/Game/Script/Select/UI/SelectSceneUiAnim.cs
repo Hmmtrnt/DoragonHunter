94e7b05aeca7e402321083839b175fef9c35251d
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2024-01-19 09:31:42 +0900

    選択画面のUIアニメーションのスクリプト追加

---
+++ MonsterHunter/Assets/Game/Script/Select/UI/SelectSceneUiAnim.cs
@@ -0,0 +1,26 @@
+/*選択画面のUIのアニメーション*/
+
+using UnityEngine;
+
+public class SelectSceneUiAnim : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    // 選択しているUIを移動した時のアニメーション
+    private void SelectUiAnim()
+    {
+
+    }
+
+    // テキストUIのアニメーション
+}

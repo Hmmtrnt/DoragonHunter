d86b70ee35a880c91a54017b8c760eb1c661575e
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2024-01-30 11:23:22 +0900

    音量調節のスライダーの枠とベースを追加

---
+++ MonsterHunter/Assets/Game/Script/Common/AudioManager.cs
@@ -0,0 +1,26 @@
+/*‰¹—Ê‚Ìˆ—*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioMixer _audioMixer;
+
+    // ‰¹—Ê’²ß‚Ìˆ×‚ÌUI.
+    public Image _BGMSlider;
+    public Image _SESlider;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}

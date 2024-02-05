f08254afcbefc303d95d018d3889e1d8af79a072
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2024-02-05 11:12:27 +0900

    クリアタイムの処理の引っ越し完了

---
+++ MonsterHunter/Assets/Game/Script/Result/RankLog.cs
@@ -0,0 +1,29 @@
+/*ƒŠƒUƒ‹ƒg‰æ–Ê‚ÌƒƒS*/
+
+using UnityEngine;
+
+public class RankLog : MonoBehaviour
+{
+    // ƒ‰ƒ“ƒN‚Ìí—Ş.
+    enum SpriteRank
+    {
+        S_RANK,
+        A_RANK,
+        B_RANK,
+        C_RANK,
+        MAX_RANK
+    }
+
+    public Sprite[] _rank;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}

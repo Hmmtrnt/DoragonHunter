6160e6be0155ce1929098ae5e6d0307d9e231c2a
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2024-02-06 18:59:08 +0900

    納刀後の回避の違和感を除去

--- MonsterHunter/Assets/Game/Script/Main/Char/Player/UnsheathedSword/StateIdle.cs
+++ MonsterHunter/Assets/Game/Script/Main/Char/Player/UnsheathedSword/StateIdle.cs
@@ -12,6 +12,12 @@ public partial class PlayerState
         public override void OnEnter(PlayerState owner, StateBase prevState)
         {
             owner._idleMotion = true;
+
+            if(prevState == _sheathingSword)
+            {
+                Debug.Log("通った");
+                owner._motionFrame = 0;
+            }
         }
 
         public override void OnUpdate(PlayerState owner)
@@ -21,7 +27,7 @@ public partial class PlayerState
 
         public override void OnFixedUpdate(PlayerState owner)
         {
-
+            owner._motionFrame++;
         }
 
         public override void OnExit(PlayerState owner, StateBase nextState)
@@ -32,8 +38,8 @@ public partial class PlayerState
         public override void OnChangeState(PlayerState owner)
         {
             // 移動.
-            if (owner._leftStickHorizontal != 0.0f ||
-                owner._leftStickVertical != 0.0f)
+            if ((owner._leftStickHorizontal != 0.0f ||
+                owner._leftStickVertical != 0.0f) && owner._motionFrame > 10)
             {
                 if (owner._input._RBButton && !owner._openMenu)
                 {

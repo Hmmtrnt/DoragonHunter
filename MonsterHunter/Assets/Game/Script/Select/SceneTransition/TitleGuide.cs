c52533aab671ea7d486ef499af8b1c0cf28a4930
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2024-02-08 16:41:36 +0900

    タイトル画面へ戻るUIを開くFlag処理追加途中

--- MonsterHunter/Assets/Game/Script/Select/SceneTransition/TitleGuide.cs
+++ MonsterHunter/Assets/Game/Script/Select/SceneTransition/TitleGuide.cs
@@ -6,13 +6,32 @@ using UnityEngine;
 
 public class TitleGuide : MonoBehaviour
 {
+    // ”ÍˆÍ“à‚É‚¢‚é‚©‚Ç‚¤‚©.
+    private bool _collisionStay = false;
+
     void Start()
     {
-        
+        _collisionStay=false;
     }
 
     void Update()
     {
         
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            _collisionStay = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            _collisionStay = false;
+        }
     }
 }

d895cc02ef230a58aa951206f39fa747db8496fd
Hmmtrnt <rento5310@gmail.com>
2024-02-22 01:59:11 +0900

    突きの再調整
    フレームレートが低いため確認不足

--- MonsterHunter/Assets/Game/Script/Main/Char/Player/DrawnSword/StatePiercing.cs
+++ MonsterHunter/Assets/Game/Script/Main/Char/Player/DrawnSword/StatePiercing.cs
@@ -6,6 +6,10 @@ public partial class Player
 {
     public class StatePiercing : StateBase
     {
+        // 一度処理を通したら次から通さない.
+        //HACK:変数名を変更.
+        private bool _test = false;
+
         public override void OnEnter(Player owner, StateBase prevState)
         {
             owner._drawnThrustSlash = true;
@@ -16,15 +20,17 @@ public partial class Player
             owner._increaseAmountRenkiGauge = 3;
             owner._hitStopTime = 0.01f;
             owner._attackCol._isOneProcess = true;
+            _test = false;
         }
 
         public override void OnUpdate(Player owner)
         {
-            if (owner._stateFlame == 15)
+            if (owner._stateTime >= 0.1f && !_test)
             {
+                _test = true;
                 owner._weaponActive = true;
             }
-            else if (owner._stateFlame == 25)
+            else if (owner._stateTime >= 0.2f)
             {
                 owner._weaponActive = false;
             }
@@ -32,7 +38,7 @@ public partial class Player
 
 
             // 前進させる.
-            if (owner._stateFlame <= 20)
+            if (owner._stateTime <= 0.3f)
             {
                 owner.ForwardStep(2);
             }
@@ -57,12 +63,12 @@ public partial class Player
         public override void OnChangeState(Player owner)
         {
             // アイドル.
-            if (owner._stateFlame >= 70)
+            if (owner._stateTime >= 1.3f)
             {
                 owner.StateTransition(_idleDrawnSword);
             }
             // 回避.
-            else if (owner._stateFlame >= owner._nextMotionFlame &&
+            else if (owner._stateTime >= 0.53f &&
                 owner._viewDirection[(int)viewDirection.FORWARD] && 
                 owner.GetDistance() > 1 &&
                 owner._input._AButtonDown)
@@ -70,7 +76,7 @@ public partial class Player
                 owner.StateTransition(_avoidDrawnSword);
             }
             // 右回避.
-            else if (owner._stateFlame >= owner._nextMotionFlame &&
+            else if (owner._stateTime >= 0.53f &&
                 owner._viewDirection[(int)viewDirection.RIGHT] && 
                 owner.GetDistance() > 1 &&
                 owner._input._AButtonDown)
@@ -78,7 +84,7 @@ public partial class Player
                 owner.StateTransition(_rightAvoid);
             }
             // 左回避.
-            else if (owner._stateFlame >= owner._nextMotionFlame &&
+            else if (owner._stateTime >= 0.53f &&
                 owner._viewDirection[(int)viewDirection.LEFT] && 
                 owner.GetDistance() > 1 
                 && owner._input._AButtonDown)
@@ -86,7 +92,7 @@ public partial class Player
                 owner.StateTransition(_leftAvoid);
             }
             // 後ろ回避.
-            else if (owner._stateFlame >= owner._nextMotionFlame &&
+            else if (owner._stateTime >= 0.53f &&
                 owner._viewDirection[(int)viewDirection.BACKWARD] &&
                 owner.GetDistance() > 1 &&
                 owner._input._AButtonDown)
@@ -94,13 +100,13 @@ public partial class Player
                 owner.StateTransition(_backAvoid);
             }
             // 斬り上げ.
-            else if (owner._stateFlame >= owner._nextMotionFlame - 10 &&
+            else if (owner._stateTime >= 0.5f &&
                 (owner._input._YButtonDown || owner._input._BButtonDown))
             {
                 owner.StateTransition(_slashUp);
             }
             // 気刃斬り1.
-            else if (owner._stateFlame >= owner._nextMotionFlame &&
+            else if (owner._stateTime >= 0.53f &&
                 owner._input._RightTrigger >= 0.5)
             {
                 owner.StateTransition(_spiritBlade1);

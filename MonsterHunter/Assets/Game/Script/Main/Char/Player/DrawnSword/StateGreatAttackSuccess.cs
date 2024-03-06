7cce13d759f7b1dc5bb8d6e016ea94fe13506b6a
Hmmtrnt <rento5310@gmail.com>
2024-03-07 05:39:59 +0900

    カウンター成功時のリファクタリング一旦終了

--- MonsterHunter/Assets/Game/Script/Main/Char/Player/DrawnSword/StateGreatAttackSuccess.cs
+++ MonsterHunter/Assets/Game/Script/Main/Char/Player/DrawnSword/StateGreatAttackSuccess.cs
@@ -6,6 +6,13 @@ public partial class PlayerState
 {
     public class StateGreatAttackSuccess : StateBase
     {
+        // 攻撃判定発生タイミング.
+        private const float _spawnColTiming = 0.1f;
+        // 攻撃判定消去タイミング.
+        private const float _eraseColTiming = 0.8f;
+        // SEを鳴らすタイミング.
+        private const float _sePlayTiming = 0.1f;
+
         public override void OnEnter(PlayerState owner, StateBase prevState)
         {
             owner._attackPower = 1000;
@@ -18,15 +25,18 @@ public partial class PlayerState
 
         public override void OnUpdate(PlayerState owner)
         {
-            if(owner._stateTime >= 0.1f && !owner._isAttackProcess)
+            if(owner._stateTime >= _spawnColTiming && !owner._isAttackProcess)
             {
                 owner._weaponActive = true;
                 owner._isAttackProcess = true;
             }
-            if(owner._stateTime >= 0.8f)
+            if(owner._stateTime >= _eraseColTiming)
             {
                 owner._weaponActive = false;
             }
+
+            // 空振り効果音再生.
+            owner.SEPlayTest(_sePlayTiming, (int)SEManager.HunterSE.MISSINGROUNDSLASH);
         }
 
         public override void OnExit(PlayerState owner, StateBase nextState)
@@ -39,7 +49,7 @@ public partial class PlayerState
 
         public override void OnChangeState(PlayerState owner)
         {
-            if (owner._stateTime >= 2.0f)
+            if (owner._stateTime >= owner._stateTransitionTime[(int)StateTransitionKinds.GREATATTACKSUCCESS])
             {
                 owner.StateTransition(_idleDrawnSword);
             }

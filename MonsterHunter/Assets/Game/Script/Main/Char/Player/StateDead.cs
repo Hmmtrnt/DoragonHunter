e780894b2228fa88d9e8f55710f20bd439061c1d
Hmmtrnt <rento5310@gmail.com>
2024-03-07 16:43:47 +0900

    やられた時の処理のリファクタリング一旦終了

--- MonsterHunter/Assets/Game/Script/Main/Char/Player/StateDead.cs
+++ MonsterHunter/Assets/Game/Script/Main/Char/Player/StateDead.cs
@@ -1,23 +1,13 @@
 ﻿/*ハンターの体力が0になった時の処理*/
 
-using UnityEngine;
-
 public partial class PlayerState
 {
     public class StateDead : StateBase
     {
+        // モーションのみ再生.
         public override void OnEnter(PlayerState owner, StateBase prevState)
         {
-            //Debug.Log("死");
-        }
-
-        public override void OnUpdate(PlayerState owner)
-        {
             owner._downMotion = true;
-        }
-        public override void OnFixedUpdate(PlayerState owner)
-        {
-            
         }
     }
 }
